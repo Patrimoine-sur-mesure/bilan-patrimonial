6ef197d83861a33d65a9886fefb59131e55be3c0
Patrimoine sur Mesure <josephbiboum@patrimoinesurmesure.net>
2026-03-08 00:27:18 +0100

    Ajout telecharge pdf

--- src/PyramidePatrimoineActuel.jsx
+++ src/PyramidePatrimoineActuel.jsx
@@ -37,7 +37,7 @@ export default function PyramidePatrimoineActuel({
     diagnostic = "Surpondération moyen terme.";
     diagnosticColor = "text-orange-600";
   } else {
-    diagnostic = "Répartition patrimoniale globalement équilibrée.";
+    diagnostic = ""; '"Répartition patrimoniale globalement équilibrée.";
   }
 
   const pct = (value) => {
