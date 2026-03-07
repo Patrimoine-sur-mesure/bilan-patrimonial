c205c613a5ad43cf71195d3eb0bbc58ab3310850
Patrimoine sur Mesure <josephbiboum@patrimoinesurmesure.net>
2026-03-07 19:32:50 +0100

    Ajout Email

--- src/FormulaireBudgetPatrimonial.jsx
+++ src/FormulaireBudgetPatrimonial.jsx
@@ -616,7 +616,30 @@ export default function FormulaireBudgetPatrimonial() {
         </div>
       </div>
 
-      <div className="mt-8 flex justify-center gap-6">
+      <div className="mt-10 w-full flex justify-center">
+		  <div className="flex gap-8">
+			
+			<button
+			  type="button"
+			  onClick={handleSave}
+			  className="border-2 border-black bg-[#e6c08f] px-8 py-3 font-semibold text-black rounded"
+			>
+			  Enregistrer
+			</button>
+
+			<button
+			  type="button"
+			  onClick={handleDownloadPdf}
+			  className="border-2 border-black bg-[#d9d9d9] px-8 py-3 font-semibold text-black rounded"
+			>
+			  Télécharger le PDF
+			</button>
+
+		   </div>
+	   </div>
+	  
+	  
+	  <div className="mt-8 flex justify-center gap-6">
 	  <button
 		type="button"
 		onClick={handleSave}
